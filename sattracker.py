da2a3b7ed34c1f65aeadf995c728ab142f030e27
Simon Murphy <simon.murphy@gmail.com>
2023-02-13 22:29:52 +0000

    Picking out passes

--- sattracker.py
+++ sattracker.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import requests
 
@@ -10,4 +11,6 @@ print(response.json()['info'])
 passes = requests.get("https://api.n2yo.com/rest/v1/satellite/radiopasses/" + sats.sats['SO-50'] + "/" +
                       mysecrets.MY_LAT + "/" + mysecrets.MY_LON + "/" + mysecrets.MY_ALT + "/1/5/&apiKey=" + mysecrets.API_KEY)
 passinfo = passes.json()
-print(passinfo)
+print (passinfo)
+for idx, satpass in enumerate(passinfo['passes']):
+    print ("Pass " + str(idx) + ": Start UTC: " + datetime.datetime.fromtimestamp(satpass['startUTC']).strftime("%Y-%m-%d %H:%M:%S+00:00 (UTC)") + " Max El: " + str(satpass['maxEl']))
